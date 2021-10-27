7c8e64be772b54b70baf2d4cac445bf8b6aa7fab
nandinichhajed <nandinichhajed08@gmail.com>
2021-10-27 16:54:49 +0530

    creating model Skill

--- portfolio/resume/admin.py
+++ portfolio/resume/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
-
+from . models import *
 # Register your models here.
+
+@admin.register(Skill)
+class SkillAdmin(admin.ModelAdmin):
+    list_display = ('id', 'name', 'score')
--- portfolio/resume/models.py
+++ portfolio/resume/models.py
@@ -1,3 +1,20 @@
 from django.db import models
-
+from django.contrib.auth.models import User
+from django.template.defaultfilters import slugify
+from ckeditor.fields import RichTextField
 # Create your models here.
+
+class Skill(models.Model):
+    name = models.CharField(max_length=50, blank=True, null=True)
+    score = models.IntegerField(default=50, blank=True, null=True)
+    image = models.FileField(blank=True, null=True, upload_to='skills')
+    is_key_skill = models.BooleanField(default=False)
+
+    class Meta:
+        verbose_name_plural = 'Skills'
+        verbose_name = 'Skill'
+    
+    def __str__(self):
+        return self.name
+    
+
