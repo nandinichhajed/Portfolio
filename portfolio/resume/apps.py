779c27f69aea4f3e2f971579a96070b856b81623
nandinichhajed <nandinichhajed08@gmail.com>
2021-10-27 16:58:12 +0530

    adding signals file to app

--- portfolio/resume/apps.py
+++ portfolio/resume/apps.py
@@ -4,3 +4,6 @@ from django.apps import AppConfig
 class ResumeConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'resume'
+    def ready(self):
+        import resume.signals
+        
