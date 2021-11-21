700778db5dbee1638ab69764ed8ca8c030e5b1a7
nandinichhajed <nandinichhajed08@gmail.com>
2021-11-21 21:43:45 +0530

    creating models, views and url for links

--- portfolio/links/models.py
+++ portfolio/links/models.py
@@ -1,3 +1,11 @@
 from django.db import models
-
+from django.contrib.auth.models import User
 # Create your models here.
+
+class Links(models.Model):
+    facebook = models.CharField(max_length=200)
+    instagram = models.CharField(max_length=200)
+    linkdin = models.CharField(max_length=200)
+    twitter = models.CharField(max_length=200)
+    github = models.CharField(max_length=200)
+    
--- portfolio/links/views.py
+++ portfolio/links/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
-
+from .models import Links
 # Create your views here.
+
+def links(request):
+    links = Links.objects.all()
+    data = {
+        'links' : links, 
+    }
+    return render(request, 'main/footer.html', data)
