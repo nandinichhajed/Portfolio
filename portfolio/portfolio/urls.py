00228059e46f671ba9a158fe00843790e7323a2c
nandinichhajed <nandinichhajed08@gmail.com>
2021-11-21 20:56:53 +0530

    creating links app

--- portfolio/portfolio/urls.py
+++ portfolio/portfolio/urls.py
@@ -21,6 +21,7 @@ from django.conf.urls.static import static
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('resume.urls', namespace="main")),
+    path('links/', include('links.urls')),
 ]
 
 if settings.DEBUG:
