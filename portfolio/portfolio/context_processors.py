aec07e0376977e842b21ca36d2c075d0940c86d0
nandinichhajed <nandinichhajed08@gmail.com>
2021-11-21 21:51:10 +0530

    taking footer to backend

--- portfolio/portfolio/context_processors.py
+++ portfolio/portfolio/context_processors.py
@@ -1,9 +1,12 @@
 from django.contrib.auth.models import User
+from links.models import Links
 
 def project_context(request):
 
 	context = {
 		'me': User.objects.first(),
+		'you': Links.objects.all()
+  
 	}
 
 	return context
